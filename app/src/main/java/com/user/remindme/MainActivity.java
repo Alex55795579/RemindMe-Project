af8f448f2934a82ca879e20b9395f8ee10049a5f
Alex55795579@gmail.com <system5579>
2017-02-06 17:59:55 +0300

    [RM-4] Added Toolbar and search button.

--- app/src/main/java/com/user/remindme/MainActivity.java
+++ app/src/main/java/com/user/remindme/MainActivity.java
@@ -2,16 +2,30 @@ package com.user.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-/**
- * Created by User on 05.02.2017.
- */
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem){
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
